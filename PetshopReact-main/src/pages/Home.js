2acc4098f42db61cf277b7f297a82fc9f1b02d7e
MateusFoltran <142942489+MateusFoltranTomaz@users.noreply.github.com>
2025-05-16 09:53:01 -0300

    Add files via upload

---
+++ PetshopReact-main/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../components/Banner";
+import Servico from "../components/Servico";
+
+const Home = () => {
+	return (
+		<div>
+			<Banner />
+			<Servico />
+		</div>
+	);
+};
+
+export default Home;
