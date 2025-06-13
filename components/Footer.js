bcd4824fbd7d23fd86350c3e2acb1faac7653f18
MateusFoltran <142942489+MateusFoltranTomaz@users.noreply.github.com>
2025-06-13 11:13:07 -0300

    Add files via upload

---
+++ components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Contato from './Contato';
+import Mapa from './Mapa';
+import DireitosAutorais from './DireitosAutorais';
+
+
+function Footer() {
+  return (
+    <div>
+      <Contato/>
+      <Mapa/>
+      <DireitosAutorais/>
+    </div>
+  )
+}
+
+export default Footer
